37322cefc97fa213b4149fad9a1bae45b97770e9
druidou41 <sheper41@hotmail.fr>
2026-08-17 15:41:37 +0200

    Passer à LibreForme Duo

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-const CACHE='libreforme-v1';const FILES=['./','./index.html','./app.css','./app.js','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='libreforme-duo-v2';const FILES=['./','./index.html','./app.css','./app.js','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
 
